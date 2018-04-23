111397daecadf079c2a38e74dc69ff49a1650daf
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Window1.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Windows;
+using System.Collections;
+// ...
+
+namespace LastRow {
+
+    public partial class Window1 : Window {
+        public Window1() {
+            InitializeComponent();
+
+            ArrayList list = new ArrayList();
+            list.Add(new TestData("1", "1"));
+            list.Add(new TestData("2", "2"));
+            list.Add(new TestData("3", "3"));
+            list.Add(new TestData("4", "4"));
+            list.Add(new TestData("5", "5"));
+            list.Add(new TestData("6", "6"));
+            list.Add(new TestData("7", "7"));
+            list.Add(new TestData("8", "8"));
+            list.Add(new TestData("9", "9"));
+            list.Add(new TestData("10", "10"));
+            list.Add(new TestData("11", "11"));
+            grid.ItemsSource = list;
+        }
+
+        private void btn_Click(object sender, RoutedEventArgs e) {
+            if (view.FocusedRowData == null)
+                textbox.Text = "No Rows Selected";
+            else 
+                if (grid.GetRowVisibleIndexByHandle(view.FocusedRowData.RowHandle.Value) == 0)
+                textbox.Text = "First Row!";
+            else 
+                if (grid.GetRowVisibleIndexByHandle(view.FocusedRowData.RowHandle.Value) == grid.VisibleRowCount - 1)
+                textbox.Text = "Last Row!";
+            else
+                textbox.Text = "Row Index: " + 
+                    grid.GetRowVisibleIndexByHandle(view.FocusedRowData.RowHandle.Value).ToString();
+        }
+    }
+
+    public class TestData {
+        public string column1 { get; set; }
+        public string column2 { get; set; }
+
+        public TestData(string str1, string str2) {
+            column1 = str1;
+            column2 = str2;
+        }
+    }
+}
---
+++ VB/Window1.xaml.vb
@@ -0,0 +1,70 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Windows
+Imports System.Collections
+' ...
+
+Namespace LastRow
+
+	Partial Public Class Window1
+		Inherits Window
+		Public Sub New()
+			InitializeComponent()
+
+			Dim list As New ArrayList()
+			list.Add(New TestData("1", "1"))
+			list.Add(New TestData("2", "2"))
+			list.Add(New TestData("3", "3"))
+			list.Add(New TestData("4", "4"))
+			list.Add(New TestData("5", "5"))
+			list.Add(New TestData("6", "6"))
+			list.Add(New TestData("7", "7"))
+			list.Add(New TestData("8", "8"))
+			list.Add(New TestData("9", "9"))
+			list.Add(New TestData("10", "10"))
+			list.Add(New TestData("11", "11"))
+			grid.ItemsSource = list
+		End Sub
+
+		Private Sub btn_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			If view.FocusedRowData Is Nothing Then
+				textbox.Text = "No Rows Selected"
+			Else
+				If grid.GetRowVisibleIndexByHandle(view.FocusedRowData.RowHandle.Value) = 0 Then
+				textbox.Text = "First Row!"
+			Else
+				If grid.GetRowVisibleIndexByHandle(view.FocusedRowData.RowHandle.Value) = grid.VisibleRowCount - 1 Then
+				textbox.Text = "Last Row!"
+			Else
+				textbox.Text = "Row Index: " & grid.GetRowVisibleIndexByHandle(view.FocusedRowData.RowHandle.Value).ToString()
+			End If
+			End If
+			End If
+		End Sub
+	End Class
+
+	Public Class TestData
+		Private privatecolumn1 As String
+		Public Property column1() As String
+			Get
+				Return privatecolumn1
+			End Get
+			Set(ByVal value As String)
+				privatecolumn1 = value
+			End Set
+		End Property
+		Private privatecolumn2 As String
+		Public Property column2() As String
+			Get
+				Return privatecolumn2
+			End Get
+			Set(ByVal value As String)
+				privatecolumn2 = value
+			End Set
+		End Property
+
+		Public Sub New(ByVal str1 As String, ByVal str2 As String)
+			column1 = str1
+			column2 = str2
+		End Sub
+	End Class
+End Namespace
