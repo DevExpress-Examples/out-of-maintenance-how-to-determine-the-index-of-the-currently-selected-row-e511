362a7d02b517c0a62794c483622a406bd1656f85
DevExpressExampleBot <fakeEmail>
2022-12-17 01:13:01 +0400

    Source auto update [skip ci]

--- VB/Window1.xaml.vb
+++ VB/Window1.xaml.vb
@@ -1,54 +1,51 @@
-﻿Imports System.Windows
+Imports System.Windows
 Imports System.Collections
-' ...
 
+' ...
 Namespace LastRow
 
-	Partial Public Class Window1
-		Inherits Window
+    Public Partial Class Window1
+        Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
+        Public Sub New()
+            Me.InitializeComponent()
+            Dim list As ArrayList = New ArrayList()
+            list.Add(New TestData("1", "1"))
+            list.Add(New TestData("2", "2"))
+            list.Add(New TestData("3", "3"))
+            list.Add(New TestData("4", "4"))
+            list.Add(New TestData("5", "5"))
+            list.Add(New TestData("6", "6"))
+            list.Add(New TestData("7", "7"))
+            list.Add(New TestData("8", "8"))
+            list.Add(New TestData("9", "9"))
+            list.Add(New TestData("10", "10"))
+            list.Add(New TestData("11", "11"))
+            Me.grid.ItemsSource = list
+        End Sub
 
-			Dim list As New ArrayList()
-			list.Add(New TestData("1", "1"))
-			list.Add(New TestData("2", "2"))
-			list.Add(New TestData("3", "3"))
-			list.Add(New TestData("4", "4"))
-			list.Add(New TestData("5", "5"))
-			list.Add(New TestData("6", "6"))
-			list.Add(New TestData("7", "7"))
-			list.Add(New TestData("8", "8"))
-			list.Add(New TestData("9", "9"))
-			list.Add(New TestData("10", "10"))
-			list.Add(New TestData("11", "11"))
-			grid.ItemsSource = list
-		End Sub
+        Private Sub btn_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            If Me.view.FocusedRowData Is Nothing Then
+                Me.textbox.Text = "No Rows Selected"
+            ElseIf Me.grid.GetRowVisibleIndexByHandle(Me.view.FocusedRowData.RowHandle.Value) = 0 Then
+                Me.textbox.Text = "First Row!"
+            ElseIf Me.grid.GetRowVisibleIndexByHandle(Me.view.FocusedRowData.RowHandle.Value) = Me.grid.VisibleRowCount - 1 Then
+                Me.textbox.Text = "Last Row!"
+            Else
+                Me.textbox.Text = "Row Index: " & Me.grid.GetRowVisibleIndexByHandle(CInt(Me.view.FocusedRowData.RowHandle.Value)).ToString()
+            End If
+        End Sub
+    End Class
 
-		Private Sub btn_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			If view.FocusedRowData Is Nothing Then
-				textbox.Text = "No Rows Selected"
-			Else
-				If grid.GetRowVisibleIndexByHandle(view.FocusedRowData.RowHandle.Value) = 0 Then
-				textbox.Text = "First Row!"
-			Else
-				If grid.GetRowVisibleIndexByHandle(view.FocusedRowData.RowHandle.Value) = grid.VisibleRowCount - 1 Then
-				textbox.Text = "Last Row!"
-			Else
-				textbox.Text = "Row Index: " & grid.GetRowVisibleIndexByHandle(view.FocusedRowData.RowHandle.Value).ToString()
-			End If
-			End If
-			End If
-		End Sub
-	End Class
+    Public Class TestData
 
-	Public Class TestData
-		Public Property column1() As String
-		Public Property column2() As String
+        Public Property column1 As String
 
-		Public Sub New(ByVal str1 As String, ByVal str2 As String)
-			column1 = str1
-			column2 = str2
-		End Sub
-	End Class
+        Public Property column2 As String
+
+        Public Sub New(ByVal str1 As String, ByVal str2 As String)
+            column1 = str1
+            column2 = str2
+        End Sub
+    End Class
 End Namespace
